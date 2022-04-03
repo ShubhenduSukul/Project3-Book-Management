14281aad36e5dc845b3148915b9c9a9cf1507c1b
ShubhenduSukul <shubhendu7870608936@mail.com>
2022-04-03 05:58:45 +0530

    final update

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://Hovappa96:Ew1mml9DEx33EJGg@cluster0.8bhyj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://ShubhenduSukul:KuVlSGTp94VKKh7E@cluster0.pbil8.mongodb.net/project3?authSource=admin&replicaSet=atlas-13oko1-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
